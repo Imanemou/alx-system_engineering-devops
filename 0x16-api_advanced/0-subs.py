73acf0f9474a7faa1e6675ac695ac6a8c74a1a79
Imanemou <imane.moustakimi1998@gmail.com>
2024-02-21 03:31:26 -0800

    task

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,14 +8,10 @@ def number_of_subscribers(subreddit):
     try:
         url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
         headers = {
-            "User-Agent": "linux:0x16.api.advanced:v1.0.0\
-            (by /u/Large_Alternative_30)"
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0\(by /u/Large_Alternative_30)"
         }
         response = requests.get(url, headers=headers, allow_redirects=False)
         if response.status_code == 404:
             return 0
         results = response.json().get("data")
         return results.get("subscribers")
-    except (Exception):
-        print('Not Found')
-        return (0)
